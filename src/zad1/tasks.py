79a5a67ce83794870ace28b011477ca2db7294e6
pokermonik <pokermonik200@gmail.com>
2026-05-05 00:01:13 +0200

    ip address fix

--- src/zad1/tasks.py
+++ src/zad1/tasks.py
@@ -2,7 +2,7 @@ from celery import Celery
 import time
 from shared.database import save_to_db
 
-celery_app = Celery('tasks', broker='redis://localhost:6379/0')
+celery_app = Celery('tasks', broker='redis://127.0.0.1:6379/0')
 
 @celery_app.task
 def process_user_task(name,surname):
